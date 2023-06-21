6a456acbdc1d87073c236a980600c2c783d3930a
unknown <cristiangc92@gmail.com>
2023-06-20 21:19:34 -0300

    Modelos, relacion de muchos a muchos y creacion de la base de datos listo.

---
+++ api/src/models/Temperament.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "temperament",
+    {
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
