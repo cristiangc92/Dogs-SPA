5137edae69aaf7d6408cd65bb5a56118b0a39672
unknown <cristiangc92@gmail.com>
2023-06-28 13:36:13 -0300

    Modificiaciones para deployar en Render.

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const getTemperaments = require("./src/controllers/temperamentController");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(process.env.PORT, async () => {
     console.log("%s cargando temperamentos...");
     await getTemperaments();
     console.log("%s listening at 3001"); // eslint-disable-line no-console
